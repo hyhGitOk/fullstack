b694c15ed37d1223a9d80a12def326664d37eec6
Selina huang <hyuehua@cn.ibm.com>
2020-05-14 15:40:06 +0800

    update auth code

--- server/eurekaServer/src/main/java/com/hyh/EurekaServerApplication.java
+++ server/eurekaServer/src/main/java/com/hyh/EurekaServerApplication.java
@@ -2,11 +2,7 @@ package com.hyh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Bean;
-
-import com.hyh.security.JwtAuthenticationTokenFilter;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -15,11 +11,4 @@ public class EurekaServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
-
-//	@Bean
-//	public FilterRegistrationBean registration(JwtAuthenticationTokenFilter filter) {
-//		FilterRegistrationBean registration = new FilterRegistrationBean(filter);
-//		registration.setEnabled(true);
-//		return registration;
-//	}
 }
--- server/zuulServer/src/main/java/com/hyh/controller/AuthController.java
+++ server/zuulServer/src/main/java/com/hyh/controller/AuthController.java
@@ -11,12 +11,15 @@ package com.hyh.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hyh.entity.TUser;
+import com.hyh.service.UserService;
 import com.hyh.utility.JwtTokenUtils;
 
 import lombok.extern.slf4j.Slf4j;
@@ -28,25 +31,34 @@ public class AuthController {
 
 	private final JwtTokenUtils jwtTokenUtils;
 
+	@Autowired
+	private UserService userService;
+
 	public AuthController(JwtTokenUtils jwtTokenUtils) {
 		this.jwtTokenUtils = jwtTokenUtils;
 	}
 
 	@GetMapping(value = "/login")
-	public String login(String user, String password) {
-		Map map = new HashMap();
-		map.put("user", user);
-		map.put("password", password);
-		return jwtTokenUtils.createToken(map);
-	}
+	public ResponseEntity login(String email, String password) {
 
-	@GetMapping(value = "/hello")
-	public String hello() {
-		return "Hello success!";
+		TUser user = userService.findByEmailPwd(email, password);
+		if (user == null) {
+			return new ResponseEntity("Email or password is invalid.", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		Map map = new HashMap();
+		map.put("user", user.getEmail());
+		map.put("password", user.getPassword());
+		String token = jwtTokenUtils.createToken(map);
+
+		Map resMap = new HashMap();
+		resMap.put("userObj", user);
+		resMap.put("token", token);
+		return ResponseEntity.ok(resMap);
 	}
 
 	@GetMapping(value = "/error")
 	public ResponseEntity tokenError() {
-		  return new ResponseEntity("token is invalid.", HttpStatus.UNAUTHORIZED);
+		return new ResponseEntity("token is invalid.", HttpStatus.UNAUTHORIZED);
 	}
 }
